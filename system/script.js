1d24da635313dce9a706b9e9d2a3cf371c961361
mazah411 <139549976+Youngsharukh47@users.noreply.github.com>
2025-01-21 08:40:12 +0300

    Add files via upload

---
+++ system/script.js
@@ -0,0 +1,66 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import {
+  getFirestore,
+  getDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firestore-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhegQv3Z2p_ZySKi6MS1PEdr6EbshVMRw",
+  authDomain: "system-4e740.firebaseapp.com",
+  projectId: "system-4e740",
+  storageBucket: "system-4e740.appspot.com",
+  messagingSenderId: "47119790502",
+  appId: "1:47119790502:web:5ddddf263fc1aa20821f67",
+  measurementId: "G-VKV35BR2FV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const db = getFirestore();
+
+onAuthStateChanged(auth, (user) => {
+  const loggedInUserId = localStorage.getItem("loggedInUserId");
+  if (loggedInUserId) {
+    console.log(user);
+    const docRef = doc(db, "users", loggedInUserId);
+    getDoc(docRef)
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          document.getElementById("loggedUserFName").innerText =
+            userData.firstName;
+          document.getElementById("loggedUserEmail").innerText = userData.email;
+          document.getElementById("loggedUserLName").innerText =
+            userData.lastName;
+        } else {
+          console.log("no document found matching id");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document");
+      });
+  } else {
+    console.log("User Id not Found in Local storage");
+  }
+});
+
+const logoutButton = document.getElementById("logout");
+
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUserId");
+  signOut(auth)
+    .then(() => {
+      window.location.href = "register.html";
+    })
+    .catch((error) => {
+      console.error("Error Signing out:", error);
+    });
+});
